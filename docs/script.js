9ca36121854d60094f7e446ded81bf9e665e0a1e
BBRRE <Bilaalbarre@hotmail.com>
2023-09-03 00:33:36 +0100

    everything works except taking a file from the user and displaying it

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,15 @@
 
 //declaring global variables
+const infoParent = document.querySelector('.info-parent')
 const content = document.querySelector('.content')
 const body = document.querySelector('body')
 const form = document.querySelector('#form1')
 const name1 = document.querySelector('#notebook-name')
 const color = document.querySelector('#notebook-color')
-let i = 0
-
+//isue with inex changing is its value get reset to 0 on each refresh
+//temp solution set i to number of notepads
+let i;
+let croyden = false
 
 const addNoteBook = document.querySelector('.add-book')
 const adbook = document.createElement('button')
@@ -30,6 +33,7 @@ form.addEventListener( 'submit', (e) => {
     e.preventDefault()
 
     drawNoteBook(name1.value,color.value,i)
+    drawNtebookContainer(i)
     addToDatabase(i)
     i = i + 1
     // Console.log(i)
@@ -65,7 +69,13 @@ async function takeFromDatabase(){
     console.log(data)
     for(j = 0; j < data.length; j++){
         console.log(data[j].name,data[j].color)
-        drawNoteBook(data[j].name,data[j].color,data[j].index)
+        if(i === undefined){
+            i = 0
+        }
+        drawNoteBook(data[j].name,data[j].color,i)
+        drawNtebookContainer(`a${i}`)
+        i = content.childElementCount
+        console.log(i)
     }
 }
 
@@ -97,9 +107,67 @@ function startUp(){
     // console.log(data[j].name,data[j].color)
     // drawNoteBook(data[j].name,data[j].color)
 
-    content.addEventListener('click', (e) => {
-        target = e.target
+    // content.addEventListener('click', (e) => {
+    //     target = e.target
         
-    })
+    // })
 
-    
+    function drawNtebookContainer(i){
+        const infoParent = document.querySelector('.info-parent')
+        const info = document.createElement('div')
+        const nav = document.createElement('div')
+        const form = document.createElement('form')
+        const input = document.createElement('input')
+        const button = document.createElement('button')
+        const imageCont = document.createElement('div')
+        const close = document.createElement('button')
+
+        info.classList.add('notebook-info')
+        nav.classList.add('navbar')
+        form.id = 'file-form'
+        input.type = 'file'
+        input.required = true
+        input.id = 'file-select'
+        input.classList.add('file')
+        input.accept = 'image/png image/jpeg'
+        button.classList.add('file-submit')
+        button.innerText = 'submit'
+        imageCont.classList.add('image-flex')
+        info.style.display = 'none'
+        close.classList.add('prev')
+        close.innerText = 'close'
+
+        infoParent.appendChild(info)
+        info.appendChild(nav)
+        info.appendChild(imageCont)
+        nav.appendChild(form)
+        form.appendChild(input)
+        form.appendChild(button)
+        nav.appendChild(close)
+
+        info.classList.add(i)
+
+    }
+
+
+content.addEventListener('click', (e) => {
+    target = e.target
+    index = target.classList[1]
+    info = document.querySelectorAll('.notebook-info')
+    info.forEach(e => {
+        if( e.classList[1] === `a${index}`){
+            e.style.display = 'block'
+            croyden = true
+
+            
+
+
+        }
+    });
+})
+
+infoParent.addEventListener('click', e => {
+    if(e.target.classList[0] === 'prev')
+    e.target.parentNode.parentNode.style.display = 'none'
+    croyden = false
+})
